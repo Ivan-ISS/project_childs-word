73590f62b291533f09e7e4736bcdada15f7ed294
Ivan-ISS <vanya5@inbox.ru>
2024-07-27 19:56:46 +0500

    project_dm-tech: product page added

--- src/Components/Cart/cart.tsx
+++ src/Components/Cart/cart.tsx
@@ -1,12 +1,12 @@
 import styles from './cart.module.scss';
-import SvgGenComponents from '../Common/SvgGenComponent/svgGenComponent';
+import CartIcon from '../Common/SvgGenComponent/svgGenComponent';
 import Item from '../Common/Item/item';
 
 export default function Cart() {
 
     return (
         <div className={styles.cart}>
-            <SvgGenComponents iconName={'cart'}/>
+            <CartIcon iconName={'cart'}/>
             <Item text={'Корзина'} value={5}/>
         </div>
     );
--- src/Components/Common/Logo/logo.tsx
+++ src/Components/Common/Logo/logo.tsx
@@ -1,17 +1,18 @@
 import styles from './logo.module.scss';
+import { SVGAttributes } from 'react';
 import { Link } from 'react-router-dom';
-import SvgGenComponents from '../../Common/SvgGenComponent/svgGenComponent';
+import LogoIcon from '../../Common/SvgGenComponent/svgGenComponent';
 
-export interface LogoProps {
+export interface LogoProps extends SVGAttributes<SVGSVGElement> {
     pathLink: string;
 }
 
-export default function Logo({ pathLink }: LogoProps) {
+export default function Logo({ pathLink, ...props }: LogoProps) {
 
     return (
         <>
             <Link to={pathLink} className={styles.link}>
-                <SvgGenComponents iconName={'logo'}/>
+                <LogoIcon { ...props } iconName={'logo'} className={styles.logo}/>
             </Link>
         </>
     );
