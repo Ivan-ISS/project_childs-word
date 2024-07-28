b1afafdb2366ff4ebb4aaadfb85e9f9a3ab1e11c
Ivan-ISS <vanya5@inbox.ru>
2024-07-28 18:44:59 +0500

    project_childs-word: the layout of the template and product page is adapted for mobile devices (custom hooks, drop-down menu and burger button are added)

--- src/Components/Cart/cart.tsx
+++ src/Components/Cart/cart.tsx
@@ -1,13 +1,15 @@
 import styles from './cart.module.scss';
 import CartIcon from '../Common/SvgGenComponent/svgGenComponent';
 import Item from '../Common/Item/item';
+import useScreenSize from '../../hooks/useScreenSize';
 
 export default function Cart() {
+    const [ screenWidth ] = useScreenSize();
 
     return (
         <div className={styles.cart}>
             <CartIcon iconName={'cart'}/>
-            <Item text={'Корзина'} value={5}/>
+            <Item text={screenWidth > 560 ? 'Корзина' : ''} value={5}/>
         </div>
     );
 }
