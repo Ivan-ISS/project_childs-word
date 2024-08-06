19c0630cdb0e577e1c274f48f458e8ce4c77e7cd
Ivan-ISS <vanya5@inbox.ru>
2024-08-06 11:37:23 +0500

    project_childs-word: infinite scroll bugs fixed

--- src/hooks/useScrollBot.ts
+++ src/hooks/useScrollBot.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export interface IUseScrollBot {
     func: () => void;
@@ -7,18 +7,21 @@ export interface IUseScrollBot {
 export default function useCloseOut({ func }: IUseScrollBot) {
     const targetElement = useRef<HTMLDivElement>(null);
     /* let check = true; */
+    const [ check, setCheck ] = useState<boolean>(true);
 
     useEffect(() => {
         const root = document.getElementById('root');
         const window = document.documentElement;
 
+        if (check && window.clientHeight > 1259) {
+            func();
+            setCheck(false);
+        }
+
         const handleScroll = () => {
 
             if (root && targetElement.current /* && check */) {
-                if (
-                    (window.clientHeight + root.scrollTop - 121 >= targetElement.current.clientHeight) ||
-                    (targetElement.current.scrollHeight <= window.clientHeight)
-                ) {
+                if (window.clientHeight + root.scrollTop - 121 >= targetElement.current.clientHeight) {
                     func();
                     /* check = false; */
                 }
