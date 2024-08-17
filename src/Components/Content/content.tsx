fd2984ca6dc5c2ecf7a77592d235aebb5bcd9633
Ivan-ISS <vanya5@inbox.ru>
2024-08-17 13:30:13 +0500

    project_childs-word: added page notFound

--- src/Components/Content/content.tsx
+++ src/Components/Content/content.tsx
@@ -4,6 +4,7 @@ import { Suspense } from 'react';
 import Products from '../Pages/Products/products';
 import LazyOrders from '../Pages/Orders/orders.lazy';
 import ProductItem from '../Pages/ProductItem/productItem';
+import NotFound from '../../Components/Pages/NotFound/notFound';
 
 export default function Content() {
 
@@ -11,6 +12,7 @@ export default function Content() {
         <Routes>
             <Route path={ routes.products() } element={ <Products/> }></Route>
             <Route path={ routes.orders() } element={ <Suspense fallback={'loading...'}><LazyOrders/></Suspense> }></Route>
+            <Route path={ '/*' } element={ <NotFound/> }></Route>
             <Route path={ `${routes.product()}/:id` } element={ <ProductItem/> }></Route>
         </Routes>
     );
